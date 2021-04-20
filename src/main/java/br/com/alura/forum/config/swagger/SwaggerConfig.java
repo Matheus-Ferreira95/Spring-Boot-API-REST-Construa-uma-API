0ab2562f39aa9e20ac4827290901acf2f2018e28
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-04-19 21:41:00 -0300

    final project 3.0

--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfig.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfig.java
@@ -20,16 +20,16 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.alura.forum"))
-                .paths(PathSelectors.ant("/**")) // como nao temos nenhum endpoint restrita, vai gerar documentacao de tudo
+                .paths(PathSelectors.ant("/**"))
                 .build()
-                .ignoredParameterTypes(Usuario.class) // ignorar qualquer endpoint com parametro relacionado ao Usuario.class, para nao mostrar dados como senha do usuario
-                .globalOperationParameters(Arrays.asList(  // parametros que eu quero que o swager apresente em todos endpoints, embora estamos passando uma lista, foi passado apenas 1 parametro
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
                                         new ParameterBuilder()
                                         .name("Authorization")
                                         .description("Header para Token JWT")
-                                        .modelRef(new ModelRef("string")) // tipo do parametro
-                                        .parameterType("header")    // no nosso caso é um header e nao um parametro no corpo da requisicao
-                                        .required(false) // parametro opcional pq tem endereços que nao é necessario informar o token
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
                                         .build()));
     }
 
